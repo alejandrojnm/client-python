e5e239594401c3ee902f58a5d33e200f032c9791
Alejandro JNM <alejandrojnm@gmail.com>
2020-05-25 10:45:33 -0400

    feat(Civo): Update the lib
    - Update the lib, to use the api url by default if CIVO_API env do not exist
    
    BREAKING CHANGE: No
    
    Signed-off-by: Alejandro JNM <alejandrojnm@gmail.com>

--- civo/utils.py
+++ civo/utils.py
@@ -6,8 +6,8 @@ File to handle all utils
 def filter_list(data: dict, filter: str) -> list:
     """
     Function to handle filter in all list of the api
+    :param filter: the filter in this format (label:Home, id:6224cd2b-d416-4e92-bdbb-db60521c8eb9)
     :param data: object json
-    :param searchs: the filter in this format (label:Home, id:6224cd2b-d416-4e92-bdbb-db60521c8eb9)
     :return: the found object in json format
     """
     filter_split = filter.split(':')
